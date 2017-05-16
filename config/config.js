280ea61f7f69c140807a461c9ce72c1e7ac77ada
wheat <wheat.mai.haoqiu@foxmail.com>
2017-05-16 18:06:45 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	port:8096,
+	vendor:[
+		"react",
+		"react-dom",
+		"react-hot-loader",
+		"react-router",
+		"redux",
+		"react-redux",
+		"prop-types",
+		"isomorphic-fetch",
+		"es6-promise",
+		"redux-thunk",
+		"classnames",
+	],
+	publicPath:"/",
+}
